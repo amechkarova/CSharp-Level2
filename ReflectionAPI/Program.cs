8a7a2518c9b223c42ff5cf590c56211c2ab7fc6e
Anelia Mechkarova <AMechkarova@nemetschek.bg>
2025-03-24 12:07:25 +0200

    Refactor and uncomment the last exercise

--- ReflectionAPI/Program.cs
+++ ReflectionAPI/Program.cs
@@ -14,7 +14,7 @@ namespace ReflectionAPI
             foreach (var type in types)
             {
                 var nspace = type.Namespace;
-                if ( nspace.StartsWith("Refl") )
+                if ( nspace!.StartsWith("Refl") )
                 {
                     Console.WriteLine(@"Type name: {0}", type.Name);
                 }
@@ -24,46 +24,46 @@ namespace ReflectionAPI
             //exercise 2
             Worker workerInstance = new Worker("aneliya", "mechkarova", 27);
             Type worker = workerInstance.GetType();
-            PropertyInfo fullNameProperty = worker.GetProperty("FullName");
-            fullNameProperty.SetValue(workerInstance, "Aneliya Emilova Mechkarova");
+            PropertyInfo? fullNameProperty = worker.GetProperty("FullName");
+            fullNameProperty?.SetValue(workerInstance, "Aneliya Emilova Mechkarova");
             Console.WriteLine(workerInstance.FullName);
 
             //exercise 3
-            MethodInfo gethCharacteristicsMethod = worker.GetMethod("GetCharacteristics");
-            gethCharacteristicsMethod.Invoke(workerInstance, new object[] { true });
+            MethodInfo? getCharacteristicsMethod = worker.GetMethod("GetCharacteristics");
+            getCharacteristicsMethod?.Invoke(workerInstance, new object[] { true });
 
-            //foreach (var type in types)
-            //{
-            //    Console.WriteLine($"{type.FullName} has {type.GetProperties()} properties");
-            //}
+            foreach (var type in types)
+            {
+                Console.WriteLine($"{type.FullName} has {type.GetProperties()} properties");
+            }
 
-            //Type workerType = typeof(Worker);
-            //MemberInfo[] membersMetaData = workerType.GetMembers();
-            //foreach (MemberInfo member in membersMetaData)
-            //{
-            //    Console.WriteLine($"{member.MemberType} {member.Name}");
-            //}
-            //FieldInfo[] fieldInfo = workerType.GetFields();
-            //foreach (FieldInfo field in fieldInfo)
-            //{
-            //    Console.WriteLine($"{field.Name} {field.FieldType}");
-            //}
+            Type workerType = typeof(Worker);
+            MemberInfo[] membersMetaData = workerType.GetMembers();
+            foreach (MemberInfo member in membersMetaData)
+            {
+                Console.WriteLine($"{member.MemberType} {member.Name}");
+            }
+            FieldInfo[] fieldInfo = workerType.GetFields();
+            foreach (FieldInfo field in fieldInfo)
+            {
+                Console.WriteLine($"{field.Name} {field.FieldType}");
+            }
 
-            //var propertyInfos = workerType.GetProperties();
-            //foreach (var propertyInfo in propertyInfos)
-            //{
-            //    Console.Write(propertyInfo.Name);
-            //    var accessors = propertyInfo.GetAccessors();
-            //    foreach (var accessor in accessors)
-            //        Console.Write(accessor.Name);
-            //}
+            var propertyInfos = workerType.GetProperties();
+            foreach (var propertyInfo in propertyInfos)
+            {
+                Console.Write(propertyInfo.Name);
+                var accessors = propertyInfo.GetAccessors();
+                foreach (var accessor in accessors)
+                    Console.Write(accessor.Name);
+            }
 
-            //MethodInfo[] methods = typeof(Worker).GetMethods(BindingFlags.Instance |
-            //BindingFlags.Public);
-            //foreach (MethodInfo info in methods)
-            //{
-            //    Console.Write(info.Name);
-            //}
+            MethodInfo[] methods = typeof(Worker).GetMethods(BindingFlags.Instance |
+            BindingFlags.Public);
+            foreach (MethodInfo info in methods)
+            {
+                Console.Write(info.Name);
+            }
         }   
 
     }
